0cc8ac2364df7e505a4be3c051f99dcd0f461dc7
Anurella <ndukwuamaka@yahoo.com>
2022-02-21 22:07:11 +0100

    First commit

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+/*! SidegigDashboard v1.0.0 | (c) 2022 Amaka | MIT License | http://github.com/cferdinandi/build-tools-boilerplate */
+(function () {
+	'use strict';
+
+
+
+})();
+//# sourceMappingURL=main.js.map
